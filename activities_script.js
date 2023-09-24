be25dd25b90e78058717a3c59ce672254788a87e
Guillaume Lebeau <guillaume.lebeau44@hotmail.fr>
2023-09-24 21:39:08 +0200

    modification code HTML, ajout 'onclick', JS fonctionnel, mais mérite une opti si possible

--- activities_script.js
+++ activities_script.js
@@ -1,15 +1,121 @@
 
-document.getElementById("sports_boxe").click(function(){
-    if (document.getElementsByClassName("card_boxe").style.display === "none"){
-        document.getElementsByClassName("card_boxe").style.display = "block";
-    }else { document.getElementsByClassName("card_boxe").style.display = "none";
-    }
-})
 
-document.getElementById("sports_cardio").click(function(){
-    if (document.getElementsByClassName("card_cardio").style.display === "none"){
-        document.getElementsByClassName("card_cardio").style.display = "block";
-    }else { document.getElementsByClassName("card_cardio").style.display = "none";
-    }
-})
+function show_card_boxe() {
+    let card_cardio = document.querySelector("div.card_cardio");
+    let card_boxe = document.querySelector("div.card_boxe");
+    let card_musculation = document.querySelector("div.card_musculation");
+    let card_yoga = document.querySelector("div.card_yoga");
+    let card_aquagym = document.querySelector("div.card_aquagym");
+    let card_aquabike = document.querySelector("div.card_aquabike");
+    if (card_boxe.style.display === "none"){
+        card_boxe.style.display = "contents";
+        card_cardio.style.display = "none";
+        card_musculation.style.display = "none";
+        card_yoga.style.display = "none";
+        card_aquagym.style.display = "none";
+        card_aquabike.style.display = "none";
+    } else { card_boxe.style.display = "none"; }
+}
+
+function show_card_cardio() {
+    let card_cardio = document.querySelector("div.card_cardio");
+    let card_boxe = document.querySelector("div.card_boxe");
+    let card_musculation = document.querySelector("div.card_musculation");
+    let card_yoga = document.querySelector("div.card_yoga");
+    let card_aquagym = document.querySelector("div.card_aquagym");
+    let card_aquabike = document.querySelector("div.card_aquabike");
+    if (card_cardio.style.display === "none"){
+        card_boxe.style.display = "none";
+        card_cardio.style.display = "contents";
+        card_musculation.style.display = "none";
+        card_yoga.style.display = "none";
+        card_aquagym.style.display = "none";
+        card_aquabike.style.display = "none";
+    } else { card_cardio.style.display = "none"; }
+}
+
+function show_card_musculation() {
+    let card_cardio = document.querySelector("div.card_cardio");
+    let card_boxe = document.querySelector("div.card_boxe");
+    let card_musculation = document.querySelector("div.card_musculation");
+    let card_yoga = document.querySelector("div.card_yoga");
+    let card_aquagym = document.querySelector("div.card_aquagym");
+    let card_aquabike = document.querySelector("div.card_aquabike");
+    if (card_musculation.style.display === "none"){
+        card_boxe.style.display = "none";
+        card_cardio.style.display = "none";
+        card_musculation.style.display = "contents";
+        card_yoga.style.display = "none";
+        card_aquagym.style.display = "none";
+        card_aquabike.style.display = "none";
+    } else { card_musculation.style.display = "none"; }
+}
+
+function show_card_yoga() {
+    let card_cardio = document.querySelector("div.card_cardio");
+    let card_boxe = document.querySelector("div.card_boxe");
+    let card_musculation = document.querySelector("div.card_musculation");
+    let card_yoga = document.querySelector("div.card_yoga");
+    let card_aquagym = document.querySelector("div.card_aquagym");
+    let card_aquabike = document.querySelector("div.card_aquabike");
+    if (card_yoga.style.display === "none"){
+        card_boxe.style.display = "none";
+        card_cardio.style.display = "none";
+        card_musculation.style.display = "none";
+        card_yoga.style.display = "contents";
+        card_aquagym.style.display = "none";
+        card_aquabike.style.display = "none";
+    } else { card_yoga.style.display = "none"; }
+}
+
+function show_card_aquagym() {
+    let card_cardio = document.querySelector("div.card_cardio");
+    let card_boxe = document.querySelector("div.card_boxe");
+    let card_musculation = document.querySelector("div.card_musculation");
+    let card_yoga = document.querySelector("div.card_yoga");
+    let card_aquagym = document.querySelector("div.card_aquagym");
+    let card_aquabike = document.querySelector("div.card_aquabike");
+    if (card_aquagym.style.display === "none"){
+        card_boxe.style.display = "none";
+        card_cardio.style.display = "none";
+        card_musculation.style.display = "none";
+        card_yoga.style.display = "none";
+        card_aquagym.style.display = "contents";
+        card_aquabike.style.display = "none";
+    } else { card_aquagym.style.display = "none"; }
+}
+
+function show_card_aquabike() {
+    let card_cardio = document.querySelector("div.card_cardio");
+    let card_boxe = document.querySelector("div.card_boxe");
+    let card_musculation = document.querySelector("div.card_musculation");
+    let card_yoga = document.querySelector("div.card_yoga");
+    let card_aquagym = document.querySelector("div.card_aquagym");
+    let card_aquabike = document.querySelector("div.card_aquabike");
+    if (card_aquabike.style.display === "none"){
+        card_boxe.style.display = "none";
+        card_cardio.style.display = "none";
+        card_musculation.style.display = "none";
+        card_yoga.style.display = "none";
+        card_aquagym.style.display = "none";
+        card_aquabike.style.display = "contents";
+    } else { card_aquabike.style.display = "none"; }
+}
+
+
+function hide() {
+    let card_boxe = document.querySelector("div.card_boxe");
+    let card_cardio = document.querySelector("div.card_cardio");
+    let card_musculation = document.querySelector("div.card_musculation");
+    let card_yoga = document.querySelector("div.card_yoga");
+    let card_aquagym = document.querySelector("div.card_aquagym");
+    let card_aquabike = document.querySelector("div.card_aquabike");
+    card_boxe.style.display = "none";
+    card_cardio.style.display = "none";
+    card_musculation.style.display = "none";
+    card_yoga.style.display = "none";
+    card_aquagym.style.display = "none";
+    card_aquabike.style.display = "none";
+}
+
 

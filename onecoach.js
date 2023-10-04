cef91194adaffd88469e79eb906e0b7e3496c545
Aurelien <aurelien.emeriau@hotmail.fr>
2023-10-04 17:19:44 +0200

    modifs footer

--- onecoach.js
+++ onecoach.js
@@ -43,8 +43,7 @@ function init() {
     function functionDisplay() {
 
         const active = document.getElementById("global").dataset.active
-        active = localStorage.getItem("sport")
-        console.log("active", active);
+    
         if (active != this.dataset.section) {
 
             let articleActive = document.getElementById("global").dataset.active; // on récupère l'élément précédent actif
